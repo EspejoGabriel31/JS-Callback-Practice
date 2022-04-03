10e416c764dcf30a97d9f7e70d0dd9e9c65f821e
unknown <gespejo2144@gmail.com>
2022-04-03 16:19:55 -0700

    added change direction functionality

--- move.js
+++ move.js
@@ -6,7 +6,7 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom){
+    function moveWithArrowKeys(left, bottom, callback){
         let direction = null;
         let x = left;
         let y = bottom;
@@ -49,10 +49,12 @@ function move(element) {
             if(e.key === 'ArrowRight'){
                 direction = 'east'
             }
+            callback(direction)
         })
 
         document.addEventListener('keyup', function(e){
             direction = null
+            callback(direction)
         })
     }
 
